49603353e87cb0a3cf2e1e6632cc912be7e585c3
wengmaxime <wengmaxime@outlook.fr>
2024-05-23 16:41:12 +0200

    create facture migration table

---
+++ GPCS/database/migrations/2024_05_23_141520_create_facture_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('facture', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('apartments_id')->references('id')->on('apartments');
+            $table->foreignId('client_id')->references('id')->on('user');
+            $table->timestamp('date_reservation_start');
+            $table->timestamp('date_reservation_end');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('facture');
+    }
+};
